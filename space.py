9b39fd0b4f9038ee1047afc8ef54cd01f2ddfaa8
Dmitriy <dchukhin@gmail.com>
2014-02-15 18:54:15 -0500

    LR-2 space.py uses a dictionary for nsew of space

--- space.py
+++ space.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
+import space_dictionary
+
 class Space(object):
     """
     A given location on the map. Connects with other spaces
     to form larger geographic areas.
     """
-    pass
+    #name is the name of the object. this gets overwritten each time a space is created.
+    self.name = 'space'
+    def __init__(self):
+        self._directions = space_dictionary.dictionary[name]
+        print self._directions
+
+
+
+def chris_room(Space):
+    self.name = 'chris_room'
+    #make sure to define the space name before the __init__ statement!!!
+    Space.__init__(self)
---
+++ space_dictionary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+"""
+This is a dictionary of dictionaries of spaces that defines what is north, east, south, and west of each space. It can be used to determine how to move between spaces.
+"""
+
+
+#chris needs to change these names to actual names, and then fill in what each direction is for each location
+def space_name1={'north':'what is north of here', 'east':'what is east of here', 'south': 'what is south of here', 'west':'what is west of here'}
+
+def space_name2={'north':'what is north of here', 'east':'what is east of here', 'south': 'what is south of here', 'west':'what is west of here'}
+
+
+
+
+def dictionary_of_spaces={'space_name1':space_name1,'space_name2':space_name2}
+
