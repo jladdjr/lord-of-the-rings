1537673a4d3925939189ec9c729fee3a737c6e16
Dmitriy <dchukhin@gmail.com>
2014-02-17 12:38:17 -0500

    LR-2 #2

--- constants.py
+++ constants.py
@@ -2,4 +2,23 @@
 Constants.
 """
 
-COMMAND_PROMPT = " > "
+COMMAND_PROMPT = "> "
+
+
+class Direction(object):
+    """
+    The cardinal directions.
+    """
+    NORTH = 'north'
+    SOUTH = 'south'
+    EAST  = 'east'
+    WEST  = 'west'
+
+
+class ItemType(object):
+    """
+    When a new item is created, its type should
+    be added here. (e.g. POTION = 1, WEAPON = 2, ARMOR = 3, etc.)
+    """
+    GENERIC = 1
+
