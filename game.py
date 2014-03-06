b83091743a531bb5a6f000cb3a5f1762c31a6081
simfarm <cs.wang20@gmail.com>
2014-03-06 14:49:38 -0500

    Added DescribeCommand

--- game.py
+++ game.py
@@ -68,8 +68,10 @@ class Game(object):
         westCmd = WestCommand("west", 
                     "Moves the player to the space west of current space")
         self._commandWords.addCommand("west", westCmd)
-
-        #TODO: Add describe command
+        
+        descCmd = DescribeCommand("describe", 
+                    "Gives description of current space", self._player)
+        self._commandWords.addCommand("describe", descCmd)
 
         #Parser
         self._parser = Parser(self._commandWords)
