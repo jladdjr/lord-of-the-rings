998102d8d9a402b755defe93dba863622c64efb2
simfarm <cs.wang20@gmail.com>
2014-05-27 11:02:58 -0400

    Testing

--- test.py
+++ test.py
@@ -1487,8 +1487,9 @@ class ShopSellItems(unittest.TestCase):
         self.assertTrue(armor in testShop._items, errorMsg)
 
         #Item prices are normal prices, not sell value
-        errorMsg = "Item costs were not set back to where they were supposed to be. %s %s" %(item.getName(), item.getCost())
+        
         for item in testShop._items:
+            errorMsg = "Item costs were not set back to where they were supposed to be. %s %s" %(item.getName(), item.getCost())
             self.assertEqual(item._cost, COST, errorMsg) 
         
         #Player chooses to: gobbledigook, quit the shop - context menus should not crash program
