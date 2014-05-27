d099f4f9a024cfff0bfb6e363cc09c3ca7d50326
simfarm <cs.wang20@gmail.com>
2014-05-27 12:54:32 -0400

    Testing

--- test.py
+++ test.py
@@ -1435,7 +1435,7 @@ class InnTest(unittest.TestCase):
         player = Player("Frodo", space)
         
         #For invalid user input
-        rawInputMock = MagicMock(side_effect = ["gobbledigook", "leave city"])
+        rawInputMock = MagicMock(side_effect = ["gobbledigook", "gobbledigook", "gobbledigook", "quit"])
         with patch('cities.inn.raw_input', create=True, new=rawInputMock):
             testInn.enter(player)
         
